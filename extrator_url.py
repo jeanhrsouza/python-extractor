e980ac452aeddc00d63283fde13f311d68667e5a
Jean Souza <jeanhrsouza@hotmail.com>
2023-04-14 21:57:05 -0300

    aula 04: expressoes regulares

--- extrator_url.py
+++ extrator_url.py
@@ -1,3 +1,4 @@
+import re
 
 class ExtratorURL:
     def __init__(self, url):
@@ -13,6 +14,11 @@ class ExtratorURL:
     def valida_url(self):
         if not self.url:
             raise ValueError("A URL está vazia")
+
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(url)
+        if not match:
+            raise ValueError("A URL não é válida")
 
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
@@ -35,8 +41,8 @@ class ExtratorURL:
         return valor
 
 
-
-extrator_url = ExtratorURL("https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar")
+url = "https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
 
---
+++ validador-url.py
@@ -0,0 +1,45 @@
+'''
+Verifica se a base da URL está de acordo com o padrão correto.
+
+Exemplos de URLs válidas:
+    bytebank.com/cambio
+    bytebank.com.br/cambio
+    www.bytebank.com/cambio
+    www.bytebank.com.br/cambio
+    http://www.bytebank.com/cambio
+    http://www.bytebank.com.br/cambio
+    https://www.bytebank.com/cambio
+    https://www.bytebank.com.br/cambio
+
+Exemplos de URLs inválidas:
+    https://bytebank/cambio
+    https://bytebank.naoexiste/cambio
+    ht://bytebank.naoexiste/cambio
+'''
+
+
+import re
+
+# [abc] pode conter qualquer um desses caracteres
+# (https://) diz que tem que conter exatamente os caracteres
+
+url = "https://www.bytebank.com.br/cambio"
+padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+
+match = padrao_url.match(url)
+
+if not match:
+    raise ValueError("A URL não é válida")
+
+print("A URL é válida")
+
+
+
+
+
+
+
+
+
+
+
